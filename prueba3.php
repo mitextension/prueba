6ec96bc8aaa302422e2b2ba00562f6349a9737fa
Juan Jose Ruvalcaba Jimenez <juan@debian-s-1vcpu-1gb-sfo2-01.localdomain>
2018-08-06 13:29:44 -0500

    Revert "Creo que le estamos hagarrando el hilo a esto"
    This reverts commit 82bbd53ac69969fecde2c1f6c7bc3aafc122dd38.

--- prueba3.php
+++ prueba3.php
@@ -1,5 +1,8 @@
 esta es la prueba 3 para enviar
 aqui edite este archivo porque habia un error
+<<<<<<< HEAD
 Ohhh esto esta super
 
 este tambien lo modifique
+=======
+>>>>>>> parent of 82bbd53... Creo que le estamos hagarrando el hilo a esto
